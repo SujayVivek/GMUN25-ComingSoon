909125161e7aba12969f509a151cb17ac9e09d57
SujayVivek <sujayv2016@gmail.com>
2024-11-13 10:15:00 +0530

    First msg

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Home } from './pages/Home'
+
+function App() {
+  return (
+    <>
+    <div className='container-main'>
+      <Home/>
+    </div>
+    </>
+  )
+}
+
+export default App
